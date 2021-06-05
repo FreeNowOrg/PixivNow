d08d6d0b4794112598439419fe00119d68b4ad70
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2021-06-06 07:14:06 +0800

    Update and rename api/byTagName/[name].ts to api/search/[word].ts

---
+++ api/search/[word].ts
@@ -0,0 +1,13 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import { request, replaceUrl } from '../utils'
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const { key } = req
+  const { word } = query
+  try {
+    const list = await request(`search/artworks/${word}`, { mode: 'safe', p: '1', ...query })
+    res.send(replaceUrl(list))
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
