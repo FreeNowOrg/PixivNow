6bd5ef9025144b3fe4aae4542276d621582553f5
Dragon-Fish <824399619@qq.com>
2021-06-06 02:03:56 +0800

    chore: minor fix

--- api/illust/[id].ts
+++ api/illust/[id].ts
@@ -12,6 +12,15 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
   try {
     const detail = await pixiv.getIllustByID(id)
+    detail.urls.map((item) => {
+      for (let index in item) {
+        item[index] = item[index].replace(
+          'https://i.pximg.net/',
+          'https://pixiv-now.vercel.app/imgProxy/'
+        )
+      }
+      return item
+    })
     return res.send(detail)
   } catch (error) {
     return res.status(500).send(error)
