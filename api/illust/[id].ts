a0e5bed8aba97fe98045063c942efbafb649c827
Dragon-Fish <824399619@qq.com>
2021-06-06 01:49:22 +0800

    chore: minor fix

--- api/illust/[id].ts
+++ api/illust/[id].ts
@@ -4,7 +4,7 @@ const pixiv = new Pixiv()
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   const id = req.query?.id as string
-  if (/^\d+$/g.test(id)) {
+  if (!/^\d+$/g.test(id)) {
     return res.status(400).send({
       error: 'Invalid id',
     })
