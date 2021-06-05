cd6a8d798a332afb971bb9d182286e62bc6ce3ae
Dragon-Fish <824399619@qq.com>
2021-06-06 01:46:00 +0800

    333

--- api/illust/[id].ts
+++ api/illust/[id].ts
@@ -1,23 +1,17 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import PixivAppApi from 'pixiv-app-api'
-const pixiv = new PixivAppApi(
-  process.env.PIXIV_USERNAME,
-  process.env.PIXIV_PASSWORD,
-  {
-    camelcaseKeys: true,
-  }
-)
+import { Pixiv } from '@ibaraki-douji/pixivts'
+const pixiv = new Pixiv()
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const id = parseInt(req.query?.id as string)
-  if (isNaN(id)) {
+  const id = req.query?.id as string
+  if (/^\d+$/g.test(id)) {
     return res.status(400).send({
       error: 'Invalid id',
     })
   }
 
   try {
-    const detail = await pixiv.illustDetail(id)
+    const detail = await pixiv.getIllustByID(id)
     return res.send(detail)
   } catch (error) {
     return res.status(500).send(error)
--- api/imgProxy/index.ts
+++ api/imgProxy/index.ts
@@ -1,23 +1,26 @@
-import axios from 'axios'
+import { Pixiv } from '@ibaraki-douji/pixivts'
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import { fromBuffer } from 'file-type'
+
+const pixiv = new Pixiv()
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   const { path } = req.query
 
-  axios
-    .get(`https://i.pximg.net${path}`, {
-      responseType: 'arraybuffer',
-      headers: {
-        'user-agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
-      },
-    })
+  pixiv
+    .download(new URL(`https://i.pximg.net${path}`))
+    // .get(`https://i.pximg.net${path}`, {
+    //   responseType: 'arraybuffer',
+    //   headers: {
+    //     'user-agent':
+    //       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    //   },
+    // })
     .then(
-      ({ data, headers }) => {
-        if (headers?.['content-type']) {
-          res.setHeader('Content-Type', headers['content-type'])
-        }
-        res.status(200).send(Buffer.from(data, 'base64'))
+      async (image) => {
+        const fileType = await fromBuffer(image)
+        res.setHeader('content-type', fileType.mime)
+        res.status(200).send(image)
       },
       (err) => {
         res.status(503).send(err)
