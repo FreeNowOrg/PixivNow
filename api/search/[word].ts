e5e39769673d3cca9ba27bc1c3600177f3d86526
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2021-06-06 07:17:37 +0800

    Update [word].ts

--- api/search/[word].ts
+++ api/search/[word].ts
@@ -2,7 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import { request, replaceUrl } from '../utils'
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const { key } = req
+  const { query } = req
   const { word } = query
   try {
     const list = await request(`search/artworks/${word}`, { mode: 'safe', p: '1', ...query })
