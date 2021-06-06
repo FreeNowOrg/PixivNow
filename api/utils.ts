52953fe96e1e321390a10eaed87ec20f44c40bf6
Dragon-Fish <824399619@qq.com>
2021-06-06 16:12:10 +0800

    chore: minor fix (bump version: 1.0.6)

--- api/utils.ts
+++ api/utils.ts
@@ -32,7 +32,7 @@ async function request(path, params = {}) {
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
       },
     })
-    res.data = replaceUrl(res.data)
+    res.data = replaceUrl(res.data?.body || res.data)
     return res
   } catch (err) {
     throw err
