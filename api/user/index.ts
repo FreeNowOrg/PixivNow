27e8cad7701c015ea7dd631152a336705977050f
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2021-07-08 02:18:56 +0800

    minor fix

--- api/user/index.ts
+++ api/user/index.ts
@@ -5,22 +5,21 @@ import { handleError, replaceUrl, request } from '../utils'
 export default async (req: VercelRequest, res: VercelResponse) => {
   const token = req.cookies.PHPSESSID || req.query.token
   if (!token) {
-    return res.status(403).send({ message: '未配置用户密钥。' })
+    return res.status(403).send({ message: '未配置用户密钥' })
   }
 
   request('get', '/', req.query, req.headers)
     .then(({ data }) => {
-      return res.send(data)
       const $ = cheerio.load(data)
       const $meta = $('#meta-global-data')
       if ($meta.length < 0 || !$meta.attr('content')) {
-        return res.status(403).send({ message: '无效用户密钥。' })
+        return res.status(403).send({ message: '无效的用户密钥' })
       }
       try {
         const { userData } = JSON.parse($meta.attr('content'))
         res.send(replaceUrl(userData))
       } catch (e) {
-        throw e
+        throw { message: '意料外的元信息' }
       }
     })
     .catch((err) => {
