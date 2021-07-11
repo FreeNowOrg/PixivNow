882e21d87ea2804a947fd3f32c6e8148c23d4b23
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-07-11 22:47:40 +0800

    add basic comment

---
+++ src/components/Comment/Comment.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.comment
+  img.avatar(
+    :src="comment.img"
+    :title="comment.userName+ ' (' + comment.userId + ')'"
+  )
+  h4 {{ comment.userName }}
+  div
+    p {{ comment.comment }}
+    .commentDate {{ comment.commentDate }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import stampList from './stampList.json'
+
+export default defineComponent({
+  props: ['comment'],
+  mounted() {
+    
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
