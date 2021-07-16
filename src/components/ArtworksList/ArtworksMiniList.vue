908b2872b5be92c72f4e5480e45b189769f529fe
Dragon-Fish <824399619@qq.com>
2021-07-16 20:51:38 +0800

    update: componet.ArtworksMiniList

--- src/components/ArtworksList/ArtworksMiniList.vue
+++ src/components/ArtworksList/ArtworksMiniList.vue
@@ -5,8 +5,18 @@ ul.artworksMiniList
     :class="{isAdContainer: item.isAdContainer}"
     )
     .thumb
-      .xRestrict.tag(v-if="item.xRestrict" title="R-18")
-          fa(icon="eye")
+      .sideTags
+        .xRestrict(v-if="item.xRestrict" title="R-18")
+            fa(icon="eye")
+        .pageCount(
+          v-if="item.pageCount"
+          :title="'共 ' + item.pageCount + ' 张'")
+          fa(icon="images")
+          | {{ item.pageCount }}
+        .bookmark(
+          v-if="!item.isAdContainer"
+          :class="{isBookmarked: item.bookmarkData}")
+          fa(icon="heart")
       router-link(
         v-if="item.id"
         :to="'/artworks/' + item.id")
@@ -15,7 +25,7 @@ ul.artworksMiniList
           :alt="item.alt"
           :title="item.alt"
           lazyload="")
-      .cover
+        .hoverTitle {{ item.title }}
     .info
       .title
         a.plain.isAdContainer(v-if="item.isAdContainer") 广告
@@ -85,6 +95,19 @@ export default defineComponent({
       top: 0
       display: block
 
+      &::before
+        content: ''
+        display: block
+        position: absolute
+        // background-color: rgba(0, 0, 0, 0.05)
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        z-index: 1
+        pointer-events: none
+        transition: all 0.4s ease-in-out
+
     img
       position: relative
       left: 0
@@ -93,23 +116,30 @@ export default defineComponent({
       height: 100%
       transition: all 0.4s ease-in-out
 
-    .cover
+
+    .hoverTitle
+      z-index: 10
+      color: #fff
       position: absolute
-      // background-color: rgba(0, 0, 0, 0.05)
-      top: 0
-      left: 0
-      width: 100%
-      height: 100%
-      z-index: 1
+      left: 50%
+      top: 50%
+      transform: translateX(-50%) translateY(-50%)
+      text-shadow: 0 0 4px #000
+      font-weight: 600
       pointer-events: none
+      opacity: 0
       transition: all 0.4s ease-in-out
-    
-    &:hover
-      .cover
-        background-color: rgba(255, 255, 255, 0.2)
+
+    &:hover a,
+    & a.router-link-active
+      &::before
+        background-color: rgba(0, 0, 0, 0.2)
 
       img
         transform: scale(1.1)
+
+      .hoverTitle
+        opacity: 1
 
   .title
     margin: 0.4rem 0
@@ -145,22 +175,45 @@ export default defineComponent({
   & + .cover
     background-color: rgba(100, 100, 100, 0.6) !important
 
-.xRestrict
+.sideTags > *
   position: absolute
+  z-index: 10
+
+  [data-icon]
+    font-size: 1em
+
+.pageCount
+  top: .4rem
+  right: .4rem
+  color: #fff
+  background-color: rgba(0, 0, 0, 0.6)
+  padding: .1rem .2rem
+  border-radius: 4px
+  font-size: 0.8rem
+
+  [data-icon]
+    margin-right: .2rem
+
+.xRestrict
   top: .4rem
   left: .4rem
   color: #fff
+  font-size: 0.8rem
   background-color: rgb(255, 0, 0, 0.8)
   width: 1.5rem
   height: 1.5rem
   border-radius: 50%
   display: flex
   align-items: center
-  z-index: 10
 
-  [data-icon]
-    margin: 0 auto
-    font-size: 0.8rem
+.bookmark
+  bottom: 0.4rem
+  right: 0.4rem
+  font-size: 1.2rem
+  color: #fff
+
+  &.isBookmarked
+    color: var(--theme-bookmark-color)
 
 .isAdContainer
   a
