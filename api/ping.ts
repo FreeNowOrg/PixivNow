24dfdbed1bea576b38c16370f5091bb0c4a4001b
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2021-07-19 02:22:05 +0800

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -6,7 +6,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   let status = true
   let error
   try {
-    await request({ path: '/user/10', params: req.headers })
+    await request({ path: '/user/10', headers: req.headers })
   } catch (err) {
     status = false
     error = err
