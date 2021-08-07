86caae167296d928848179c19f9e8d585dde2eb9
机智的小鱼君 <dragon-fish@qq.com>
2021-08-07 20:47:45 +0800

    hold your horse

--- api/illust/random.ts
+++ api/illust/random.ts
@@ -29,41 +29,40 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     return handleError(err, res)
   }
 
-  const list: ArtworkOrAd[] = data?.illusts || []
-  list.forEach((item, index) => {
-    if (item.isAdContainer) {
-      list.splice(index, 1)
-      return
-    }
+  let list: ArtworkOrAd[] = data?.illusts || []
+  list = list
+    .filter(({ isAdContainer }) => !isAdContainer)
+    .map((item) => {
+      // 将时间转换为日本时区，东 9 区
+      let date = new Date(item.createDate)
+      const targetTimezone = -9
+      // date 与机器时间相差的分钟数
+      const minDiff = date.getTimezoneOffset()
+      // 转换
+      date = new Date(
+        date.getTime() + minDiff * 60 * 1000 - targetTimezone * 60 * 60 * 1000
+      )
 
-    // 将时间转换为日本时区，东 9 区
-    let date = new Date(item.createDate)
-    const targetTimezone = -9
-    // date 与机器时间相差的分钟数
-    const minDiff = date.getTimezoneOffset()
-    // 转换
-    date = new Date(
-      date.getTime() + minDiff * 60 * 1000 - targetTimezone * 60 * 60 * 1000
-    )
+      // 直接从 thumb 替换，并不优雅 ×
+      // const middle = item.url
+      //   .replace(/^\/-/, '')
+      //   .replace(/^\/c\/.+?\//, '/')
+      //   .replace(/^\/(custom-thumb|img-master)/, '')
+      //   .replace(/^\//, '')
+      //   .split('_p0_')[0]
+      // 由时间生成
+      const middle = `img/${dateFormat('yyyy/MM/dd/hh/mm/ss', date)}/${item.id}`
 
-    // 直接从 thumb 替换，并不优雅 ×
-    // const middle = item.url
-    //   .replace(/^\/-/, '')
-    //   .replace(/^\/c\/.+?\//, '/')
-    //   .replace(/^\/(custom-thumb|img-master)/, '')
-    //   .replace(/^\//, '')
-    //   .split('_p0_')[0]
-    // 由时间生成
-    const middle = `img/${dateFormat('yyyy/MM/dd/hh/mm/ss', date)}/${item.id}`
+      item.urls = {
+        mini: `/-/c/48x48/img-master/${middle}_p0_square1200.jpg`,
+        thumb: `/-/c/250x250_80_a2/img-master/${middle}_p0_square1200.jpg`,
+        small: `/-/c/540x540_70/img-master/${middle}_p0_master1200.jpg`,
+        regular: `/-/img-master/${middle}_p0_master1200.jpg`,
+        original: `/-/img-original/${middle}_p0.jpg`,
+      }
 
-    item.urls = {
-      mini: `/-/c/48x48/img-master/${middle}_p0_square1200.jpg`,
-      thumb: `/-/c/250x250_80_a2/img-master/${middle}_p0_square1200.jpg`,
-      small: `/-/c/540x540_70/img-master/${middle}_p0_master1200.jpg`,
-      regular: `/-/img-master/${middle}_p0_master1200.jpg`,
-      original: `/-/img-original/${middle}_p0.jpg`,
-    }
-  })
+      return item
+    })
 
   // return image
   if (isImage) {
