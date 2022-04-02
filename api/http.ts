52298375ea18b282ef74504d41a3279e0f79dc87
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2022-04-02 20:44:23 +0800

    test fix

--- api/http.ts
+++ api/http.ts
@@ -3,21 +3,20 @@ import { Method } from 'axios'
 import { request } from './utils'
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const { __PREFIX, __PATH } = req.query
-  request({
-    method: req.method as Method,
-    path: `/${__PREFIX}${__PATH ? '/' + __PATH : ''}`,
-    params: req.query,
-    data: req.body,
-    headers: req.headers,
-  }).then(
-    ({ data }) => {
-      res.status(200).send(data)
-    },
-    (err) => {
-      return res
-        .status(err?.response?.status || 500)
-        .send(err?.response?.data || err)
-    }
-  )
+  try {
+    const { __PREFIX, __PATH } = req.query
+    const path = encodeURIComponent(
+      `/${__PREFIX}${__PATH ? '/' + __PATH : ''}`
+    ) as `/${string}`
+    const data = await request({
+      method: req.method as Method,
+      path,
+      params: req.query,
+      data: req.body,
+      headers: req.headers,
+    })
+    res.status(200).send(data)
+  } catch (err) {
+    res.status(err?.response?.status || 500).send(err?.response?.data || err)
+  }
 }
