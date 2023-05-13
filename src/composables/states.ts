a80d979f2088f5abc513326fdcf2cd11e50374d3
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2023-05-13 19:07:07 +0800

    chore: refactor store to use setup style

--- src/composables/states.ts
+++ src/composables/states.ts
@@ -1,56 +1,44 @@
 import { defineStore } from 'pinia'
 import { PixivUser } from '@/types'
 
-export const useSideNavStore = defineStore('sidenav', {
-  state: () => ({ openState: false }),
-  getters: {
-    isOpened(state) {
-      return state.openState
-    },
-  },
-  actions: {
-    toggle() {
-      this.openState = !this.openState
-    },
-    open() {
-      this.openState = true
-    },
-    close() {
-      this.openState = false
-    },
-  },
+export const useSideNavStore = defineStore('sidenav', () => {
+  const openState = ref(false)
+  const isOpened = computed(() => openState.value)
+  function toggle() {
+    openState.value = !openState.value
+  }
+  function open() {
+    openState.value = true
+  }
+  function close() {
+    openState.value = false
+  }
+  return { openState, isOpened, toggle, open, close }
 })
 
-export const useUserStore = defineStore('user', {
-  state: () => {
-    return { user: null } as { user: PixivUser | null }
-  },
-  getters: {
-    isLoggedIn(state) {
-      return !!state.user
-    },
-    userId(state) {
-      return state.user?.id
-    },
-    userName(state) {
-      return state.user?.name
-    },
-    userPixivId(state) {
-      return state.user?.pixivId
-    },
-    userProfileImg(state) {
-      return state.user?.profileImg
-    },
-    userProfileImgBig(state) {
-      return state.user?.profileImgBig
-    },
-  },
-  actions: {
-    login(userData: PixivUser) {
-      this.user = userData
-    },
-    logout() {
-      this.user = null
-    },
-  },
+export const useUserStore = defineStore('user', () => {
+  const user = ref<PixivUser | null>(null)
+  const isLoggedIn = computed(() => !!user.value)
+  const userId = computed(() => user.value?.id)
+  const userName = computed(() => user.value?.name)
+  const userPixivId = computed(() => user.value?.pixivId)
+  const userProfileImg = computed(() => user.value?.profileImg)
+  const userProfileImgBig = computed(() => user.value?.profileImgBig)
+  function login(data: PixivUser) {
+    user.value = data
+  }
+  function logout() {
+    user.value = null
+  }
+  return {
+    user,
+    isLoggedIn,
+    userId,
+    userName,
+    userPixivId,
+    userProfileImg,
+    userProfileImgBig,
+    login,
+    logout,
+  }
 })
