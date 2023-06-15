f6995021442f4cab8b2dffe0584062bcd7c57460
机智的小鱼君 <44761872+dragon-fish@users.noreply.github.com>
2023-06-15 18:47:54 +0800

    chore: add google analytics (#46)
    * chore: add google analytics
    
    * chore: use vue-gtag to add GA
    
    * revert: "chore: add google analytics"
    
    * chore: enable auto tracking
    
    ---------
    
    Co-authored-by: MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
    Co-authored-by: Dragon-Fish <xiaoyujun@aojiaogame.com>

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,7 @@
 import { router } from './router'
 import { loadLocaleMessages, setupI18n } from './i18n'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 import type { App } from 'vue'
 
 export async function registerPlugins(app: App) {
@@ -9,6 +10,13 @@ export async function registerPlugins(app: App) {
   app.use(i18n)
   app.use(router)
   app.use(createPinia())
+  app.use(
+    VueGtag,
+    {
+      config: { id: 'G-JN62ZN3RD5' },
+    },
+    router
+  )
 
   await loadLocaleMessages(i18n, initialLocale)
 }
