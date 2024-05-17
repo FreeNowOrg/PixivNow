bc0486098648fd86817dac0b4f92a81c7253a58e
NekoCharm <34984380+AlPha5130@users.noreply.github.com>
2024-05-17 10:41:24 +0800

    chore: update ugoira.vue

--- pages/ugoira.vue
+++ pages/ugoira.vue
@@ -10,10 +10,9 @@
 
 <script lang="ts" setup>
 import { NButton, NInputGroup, NInputNumber } from 'naive-ui'
-import { UgoiraPlayer, type UgoiraMeta } from '@/utils/UgoiraPlayer'
-import { IllustType, type Artwork } from '@/types'
-import UgoiraViewer from '@/components/UgoiraViewer.vue'
-import { ajax } from '@/utils/ajax'
+import { UgoiraPlayer, type UgoiraMeta } from '~/utils/UgoiraPlayer'
+import { IllustType, type Artwork } from '~/types'
+import UgoiraViewer from '~/components/UgoiraViewer.vue'
 
 const illustId = ref(116442667)
 const illust = ref<Artwork>()
@@ -26,9 +25,7 @@ async function handleInit(originalQuality?: boolean) {
   meta.value = undefined
   isLoading.value = true
 
-  illust.value = await (
-    await ajax.get(`/ajax/illust/${illustId.value}?full=1`)
-  ).data
+  illust.value = await $fetch(`/ajax/illust/${illustId.value}?full=1`)
 
   isLoading.value = false
 }
